81470e575afcf9755d9a49492391b9e23a6975a3
Heitor A Neto <heitorh3@gmail.com>
2018-11-30 13:34:35 -0200

    Propriedades / children

--- src/main.js
+++ src/main.js
@@ -3,16 +3,23 @@ import { render } from "react-dom";
 
 class Botao extends Component {
   render() {
-    return <a href="">Salvar</a>;
+    return (
+      <a href="" onClick={this.handleClick()}>
+        {this.props.title}
+      </a>
+    );
   }
 }
 
 class App extends Component {
+  handleClick() {
+    alert("Botão clicado");
+  }
   render() {
     return (
       <Fragment>
         <h1>Hello word!!</h1>
-        <Botao />
+        <Botao title="enviar" onClick={this.handleClick()} />
       </Fragment>
     );
   }
